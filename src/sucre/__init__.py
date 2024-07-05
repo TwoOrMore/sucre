71cf8ca5fc8348d71b5573107da1d4a6f0e52279
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-07-05 20:36:36 +0100

    [Feature] We can get a success callback

--- src/sucre/__init__.py
+++ src/sucre/__init__.py
@@ -6,6 +6,21 @@ class HttpCaller:
     def __init__(self, url, method):
         self._url = url
         self._method = method
+        self._success_handler = self._default_success_handler
+
+    def _default_success_handler(self, response):
+        return response
 
     def call(self, **kwargs):
-        return self._method(self._url, **kwargs)
+        response = self._method(self._url, **kwargs)
+
+        if response.status_code == 200:
+            self._success_handler(response)
+
+    @property
+    def success_handler(self):
+        return self._success_handler
+
+    @success_handler.setter
+    def success_handler(self, callback):
+        self._success_handler = callback
--- tests/test_http_caller.py
+++ tests/test_http_caller.py
@@ -4,10 +4,32 @@ from requests import Response
 from sucre import HttpCaller
 
 
+def setup_response(
+    mocker,
+    request_mock_method,
+    response,
+    status_code=200,
+    status_side_effect=[],
+    text="",
+):
+    response_mock = mocker.MagicMock(spec=Response)
+    if isinstance(response, list):
+        response_mock.json.side_effect = response
+    else:
+        response_mock.json.return_value = response
+    if isinstance(status_code, list):
+        type(response_mock).status_code = mocker.PropertyMock(side_effect=status_code)
+    else:
+        type(response_mock).status_code = mocker.PropertyMock(return_value=status_code)
+    type(response_mock).text = mocker.PropertyMock(return_value=text)
+    request_mock_method.return_value = response_mock
+    return response_mock
+
+
 def test_request(mocker):
     url = "http://some_url.com"
     get_mock = mocker.patch("requests.get")
-    get_mock.return_value = {}
+    setup_response(mocker, get_mock, {})
     caller = HttpCaller(url, requests.get)
 
     caller.call()
@@ -18,7 +40,7 @@ def test_request(mocker):
 def test_request_with_kwargs(mocker):
     url = "http://some_url.com"
     get_mock = mocker.patch("requests.get")
-    get_mock.return_value = {}
+    setup_response(mocker, get_mock, {})
     caller = HttpCaller(url, requests.get)
 
     caller.call(headers={}, auth={})
@@ -26,14 +48,15 @@ def test_request_with_kwargs(mocker):
     get_mock.assert_called_once_with(url, headers={}, auth={})
 
 
-def test_we_get_a_response(mocker):
+def test_we_get_a_valid_response(mocker):
     url = "http://some_url.com"
     get_mock = mocker.patch("requests.get")
-    response_mock = mocker.MagicMock(spec=Response)
-    get_mock.return_value = response_mock
+    response_mock = setup_response(mocker, get_mock, {})
+    stub = mocker.stub(name="successful call")
     caller = HttpCaller(url, requests.get)
+    caller.success_handler = stub
 
-    actual_response = caller.call(headers={}, auth={})
+    caller.call(headers={}, auth={})
 
     get_mock.assert_called_once_with(url, headers={}, auth={})
-    assert isinstance(actual_response, Response)
+    stub.assert_called_once_with(response_mock)
